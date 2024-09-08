61c30197645ac9eb0e27070956a375ab5d6fe75c
grimransh <anuragpal572004@gmail.com>
2024-09-08 22:43:32 +0530

    Create Sum of all items in a dictionary.py

---
+++ Sum of all items in a dictionary.py
@@ -0,0 +1,7 @@
+""" sum of values in dictionary"""
+
+dic={ 'a':455, 'b':223, 'c':300, 'd':908, 'e':456, 'f':1000, 'g':567, 'h':239, 'i':1000, 'j':1000}
+
+print("Dictionary: ", dic)
+
+print("sum of values in above dict: ",sum(dic.values()))
