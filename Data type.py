aa4df39f38bfad573f0c94df8cea038d970b6c8e
grimransh <anuragpal572004@gmail.com>
2024-09-21 23:31:13 +0530

    Create Data type.py

---
+++ Data type.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 22:45:37 2024
+
+@author: ANURAG PAL
+"""
+
+""" The most commonly used data types include """
+
+# NUMBERIC
+#integer
+x=1
+print("Type of\n",x,"\n",type(x))
+#float
+x=5.4
+print("Type of\n",x,"\n",type(x))
+#complex
+x=2+4j
+print("Type of\n",x,"\n",type(x))
+
+# SEQUENCE
+#string
+x="Made glorious summer by this sun of York"
+print("Type of\n",x,"\n",type(x))
+#list
+x=["apple", "banana", "cherry","1","0.2"]
+print("Type of\n",x,"\n",type(x))
+#tupple
+x=(1, 2, 3)
+print("Type of\n",x,"\n",type(x))
+
+# MAPPING
+#dictionary
+x={1:"name1",2:"name2",3:"name3"}
+print("Type of\n",x,"\n",type(x))
+
+# SET
+#set
+x={"Amadeu", "Immaculada"}
+print("Type of\n",x,"\n",type(x))
+#frozenset
+x=frozenset({"Amadeu", "Immaculada"})
+print("Type of\n",x,"\n",type(x))
+
+# BOOLEAN
+#bool
+x=True
+print("Type of\n",x,"\n",type(x))
+
+# NONE
+#nonetype
+x=None
+print("Type of\n",x,"\n",type(x))
