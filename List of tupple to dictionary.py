622c2fd64c697b9455be0c57ab3b64186aacddc9
grimransh <anuragpal572004@gmail.com>
2024-10-09 22:53:32 +0530

    Create List of tupple to dictionary.py

---
+++ List of tupple to dictionary.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 21:40:38 2024
+
+@author: ANURAG PAL
+"""
+
+""" program to comvert list of tupple into dictonary"""
+
+list_of_tupple=[(1,'A'),(2,'B'),(3,'C'),(4,'E')]
+
+dictionary=dict(list_of_tupple)
+
+print('dictionary is:',dictionary)
