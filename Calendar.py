9e3cf43fc818d3e3255f3e5a3b70441f17883bf6
grimransh <anuragpal572004@gmail.com>
2024-10-18 18:05:16 +0530

    Create Calendar.py

---
+++ Calendar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 17:59:28 2024
+
+@author: ANURAG PAL
+"""
+
+""" CALENDAR """
+
+import calendar
+
+#printing the calendar of oct 2024
+cal = calendar.month(2024,10)
+print(cal)
+
+# printing the calendar of year 2024
+s = calendar.prcal(2024) 
+print(s)
